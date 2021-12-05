87e5950477e82bc937ea1ad3e17ea218f81b9f6a
Aimmzz <rohimkurniawan456@gmail.com>
2021-12-05 21:18:09 +0700

    membuat projek uas

---
+++ db/projectpos.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Des 2021 pada 15.13
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projectpos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akses`
+--
+
+CREATE TABLE `akses` (
+  `id_akses` int(11) NOT NULL,
+  `nama_akses` varchar(25) NOT NULL,
+  `deskripsi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `akses`
+--
+
+INSERT INTO `akses` (`id_akses`, `nama_akses`, `deskripsi`) VALUES
+(1, 'administrator', 'sebagai  pengelola kendali penuh pada sistem aplikasi'),
+(2, 'Asisten admin', 'sebagai pengelola sistem stok barang, penjualan dan laporan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `ukuran` varchar(5) NOT NULL,
+  `harga` int(20) NOT NULL,
+  `foto` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_kategori`, `ukuran`, `harga`, `foto`) VALUES
+(75, 'Kemeja Hitam', 21, ' 3', 150000, '42fbfa8051340169a95b04cb132d4478.jpg'),
+(76, 'Die Art', 22, ' 4', 135000, 'df9fe32484b9ba5bfb4b205b5e3fa829.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(21, 'Kemeja'),
+(22, 'Celana Jeans');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `operator`
+--
+
+CREATE TABLE `operator` (
+  `id_operator` int(11) NOT NULL,
+  `nama_operator` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `id_akses` int(3) NOT NULL,
+  `last_login` date NOT NULL,
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `operator`
+--
+
+INSERT INTO `operator` (`id_operator`, `nama_operator`, `username`, `password`, `id_akses`, `last_login`, `foto`) VALUES
+(10, 'jeff', 'jeff', '166ee015c0e0934a8781e0c86a197c6e', 1, '0000-00-00', '13005384ce54754b3a763180f2a6c83e.png'),
+(15, 'Rohim', '10200041', 'a906449d5769fa7361d7ecc6aa3f6d28', 1, '2021-12-05', '2e6b0b587369cedb86f7b1d67c761e4b.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `stok`
+--
+
+CREATE TABLE `stok` (
+  `id_stok` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `stok_barang` int(11) NOT NULL,
+  `tanggal_stok` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `stok`
+--
+
+INSERT INTO `stok` (`id_stok`, `id_barang`, `stok_barang`, `tanggal_stok`) VALUES
+(57, 75, 40, '2021-12-05'),
+(58, 76, 60, '2021-12-05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ukuran`
+--
+
+CREATE TABLE `ukuran` (
+  `id_ukuran` int(11) NOT NULL,
+  `nama_ukuran` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `ukuran`
+--
+
+INSERT INTO `ukuran` (`id_ukuran`, `nama_ukuran`) VALUES
+(1, 'S'),
+(2, 'M'),
+(3, 'L'),
+(4, 'XL'),
+(5, 'XXL'),
+(6, 'No Size');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  ADD PRIMARY KEY (`id_akses`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `operator`
+--
+ALTER TABLE `operator`
+  ADD PRIMARY KEY (`id_operator`);
+
+--
+-- Indeks untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  ADD PRIMARY KEY (`id_stok`);
+
+--
+-- Indeks untuk tabel `ukuran`
+--
+ALTER TABLE `ukuran`
+  ADD PRIMARY KEY (`id_ukuran`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT untuk tabel `operator`
+--
+ALTER TABLE `operator`
+  MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT untuk tabel `ukuran`
+--
+ALTER TABLE `ukuran`
+  MODIFY `id_ukuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
