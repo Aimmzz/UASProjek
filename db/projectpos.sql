b4fa54a18b6013404b8ff2729e2f38923c24416c
Aimmzz <rohimkurniawan456@gmail.com>
2022-06-05 14:03:39 +0700

    mengganti database

--- db/projectpos.sql
+++ db/projectpos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Des 2021 pada 15.13
+-- Waktu pembuatan: 05 Jun 2022 pada 09.00
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 7.3.27
 
@@ -44,6 +44,28 @@ INSERT INTO `akses` (`id_akses`, `nama_akses`, `deskripsi`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `bank`
+--
+
+CREATE TABLE `bank` (
+  `id` int(11) NOT NULL,
+  `nama_bank` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `bank`
+--
+
+INSERT INTO `bank` (`id`, `nama_bank`) VALUES
+(1, 'MANDIRI'),
+(2, 'BNI'),
+(3, 'BCA'),
+(4, 'BRI'),
+(5, 'CIMB Niaga');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `barang`
 --
 
@@ -61,8 +83,45 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_kategori`, `ukuran`, `harga`, `foto`) VALUES
-(75, 'Kemeja Hitam', 21, ' 3', 150000, '42fbfa8051340169a95b04cb132d4478.jpg'),
-(76, 'Die Art', 22, ' 4', 135000, 'df9fe32484b9ba5bfb4b205b5e3fa829.jpg');
+(75, 'Kemeja Hitam', 21, '3', 150000, '15371233a06364150f70276884f7707b.jpg'),
+(77, 'kemeja1', 26, ' 4', 100000, '39561e7f7755ba49cd55430f9ad6a616.jpg'),
+(78, 'Die Art', 27, ' 3', 120000, 'b2f8231d1f5dea6d1eb1b039cd282ce7.jpg'),
+(79, 'Kaos', 0, ' 1', 100000, 'bd1a802738bf52f91f9a73a24492dd81.jpg'),
+(80, 'Die Art', 21, ' 2', 150000, 'dfa8c048f3fe60e749ed4887d6b84265.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id` int(11) NOT NULL,
+  `no_trf` varchar(50) NOT NULL,
+  `nama_pelanggan` varchar(35) NOT NULL,
+  `totalpure` bigint(20) NOT NULL,
+  `grand_total` bigint(20) NOT NULL,
+  `diskon` int(3) NOT NULL,
+  `bayar` bigint(20) NOT NULL,
+  `kembalian` bigint(20) NOT NULL,
+  `catatan` varchar(50) NOT NULL,
+  `tgl_trf` date NOT NULL,
+  `jam_trf` time NOT NULL,
+  `id_pembayaran` int(2) NOT NULL,
+  `no_rek` int(18) DEFAULT NULL,
+  `atas_nama` varchar(35) NOT NULL,
+  `id_bank` int(2) DEFAULT NULL,
+  `operator` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `detail_penjualan`
+--
+
+INSERT INTO `detail_penjualan` (`id`, `no_trf`, `nama_pelanggan`, `totalpure`, `grand_total`, `diskon`, `bayar`, `kembalian`, `catatan`, `tgl_trf`, `jam_trf`, `id_pembayaran`, `no_rek`, `atas_nama`, `id_bank`, `operator`) VALUES
+(15, 'C20220603001', 'asep', 120000, 120000, 0, 200000, 80000, 'beli', '2022-06-03', '09:53:00', 1, 0, '', NULL, '10200041'),
+(16, 'C20220603015', 'asep', 120000, 120000, 0, 200000, 80000, 'beli', '2022-06-03', '09:57:22', 1, 0, '', NULL, '10200041'),
+(17, 'C20220603016', 'fahmi', 120000, 120000, 0, 300000, 180000, 'beli', '2022-06-03', '09:59:59', 1, 0, '', NULL, '10200041');
 
 -- --------------------------------------------------------
 
@@ -81,7 +140,8 @@ CREATE TABLE `kategori` (
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 (21, 'Kemeja'),
-(22, 'Celana Jeans');
+(26, 'kemeja lengan pendek'),
+(27, 'Celana Jeans');
 
 -- --------------------------------------------------------
 
@@ -104,8 +164,53 @@ CREATE TABLE `operator` (
 --
 
 INSERT INTO `operator` (`id_operator`, `nama_operator`, `username`, `password`, `id_akses`, `last_login`, `foto`) VALUES
-(10, 'jeff', 'jeff', '166ee015c0e0934a8781e0c86a197c6e', 1, '0000-00-00', '13005384ce54754b3a763180f2a6c83e.png'),
-(15, 'Rohim', '10200041', 'a906449d5769fa7361d7ecc6aa3f6d28', 1, '2021-12-05', '2e6b0b587369cedb86f7b1d67c761e4b.jpg');
+(15, 'Rohim', '10200041', 'a906449d5769fa7361d7ecc6aa3f6d28', 1, '2022-06-05', '2e6b0b587369cedb86f7b1d67c761e4b.jpg'),
+(17, 'Qurota', '10200043', 'a906449d5769fa7361d7ecc6aa3f6d28', 1, '0000-00-00', '07395ae3579d30091a9ee72a3a603f5f.png'),
+(18, 'Gloria', '10200072', 'a906449d5769fa7361d7ecc6aa3f6d28', 1, '0000-00-00', '18f4c5b2d974e96812872ecba8bf9842.png'),
+(19, 'coba', '1234', '81dc9bdb52d04dc20036dbd8313ed055', 1, '2021-12-20', '0cd3ffe8d98833c68cf23d2f4846c1c3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_byr` int(2) NOT NULL,
+  `metode` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_byr`, `metode`) VALUES
+(1, 'Cash'),
+(2, 'Transfer');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_dtlpen` int(5) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah_stok` int(11) NOT NULL,
+  `harga_barang` bigint(20) NOT NULL,
+  `sub_total` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_transaksi`, `id_dtlpen`, `id_barang`, `jumlah_stok`, `harga_barang`, `sub_total`) VALUES
+(50, 15, 78, 1, 120000, 120000),
+(51, 16, 78, 1, 120000, 120000),
+(52, 17, 78, 1, 120000, 120000);
 
 -- --------------------------------------------------------
 
@@ -125,8 +230,10 @@ CREATE TABLE `stok` (
 --
 
 INSERT INTO `stok` (`id_stok`, `id_barang`, `stok_barang`, `tanggal_stok`) VALUES
-(57, 75, 40, '2021-12-05'),
-(58, 76, 60, '2021-12-05');
+(57, 75, 40, '2021-12-20'),
+(59, 77, 20, '2021-12-20'),
+(61, 78, 27, '2022-06-03'),
+(62, 79, 20, '2022-06-03');
 
 -- --------------------------------------------------------
 
@@ -162,10 +269,22 @@ ALTER TABLE `akses`
   ADD PRIMARY KEY (`id_akses`);
 
 --
+-- Indeks untuk tabel `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `kategori`
@@ -178,6 +297,18 @@ ALTER TABLE `kategori`
 --
 ALTER TABLE `operator`
   ADD PRIMARY KEY (`id_operator`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_byr`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_transaksi`);
 
 --
 -- Indeks untuk tabel `stok`
@@ -202,28 +333,52 @@ ALTER TABLE `akses`
   MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT untuk tabel `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT untuk tabel `operator`
 --
 ALTER TABLE `operator`
-  MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_byr` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT untuk tabel `stok`
 --
 ALTER TABLE `stok`
-  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT untuk tabel `ukuran`
